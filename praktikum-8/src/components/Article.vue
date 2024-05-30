47c4b8cb6f84d933f10be0ac2cee869599d50e77
RachmasariAR <nisa011104@gmail.com>
2024-05-30 14:52:32 +0700

    coba vue

---
+++ praktikum-8/src/components/Article.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        
+        <h1>Ini adalah komponen artikel</h1>
+        <p>Ini adalah isi paragraf</p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png" alt="" width="200px">
+    </div>
+</template>
---
+++ praktikum-8/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Article from '../components/Article.vue' 
+</script>
+
+<template>
+  <main>
+    <Article />
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
